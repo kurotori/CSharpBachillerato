2f7650d481338696ee47bfe754713a3f0e442d4e
Luis Sebastián de los Angeles <luissebastianangeles@gmail.com>
2020-06-25 18:47:31 -0300

    Actualización de código

--- 04 - JuegoDeCartasGTK/JuegoDeCartasGTK/JuegoDeCartasGTK/MainWindow.cs
+++ 04 - JuegoDeCartasGTK/JuegoDeCartasGTK/JuegoDeCartasGTK/MainWindow.cs
@@ -1,15 +1,11 @@
 ﻿using System;
 using Gtk;
-using Glade;
 
 public partial class MainWindow : Gtk.Window
 {
     public MainWindow() : base(Gtk.WindowType.Toplevel)
     {
-        Glade.XML gxml = new XML(null, "GUI/prueba.glade", "window1", null);
-        gxml.Autoconnect(this);
-        ShowAll();
-        //Build();
+        Build();
     }
 
     protected void OnDeleteEvent(object sender, DeleteEventArgs a)
--- 04 - JuegoDeCartasGTK/JuegoDeCartasGTK/JuegoDeCartasGTK/Program.cs
+++ 04 - JuegoDeCartasGTK/JuegoDeCartasGTK/JuegoDeCartasGTK/Program.cs
@@ -8,7 +8,6 @@ namespace JuegoDeCartasGTK
         public static void Main(string[] args)
         {
             Application.Init();
-            //Principal win = new Principal();
             MainWindow win = new MainWindow();
             win.Show();
             Application.Run();
