d0f5589dc540b439482842c2b3d881ce5993c262
Luis Sebastián de los Angeles <luissebastianangeles@gmail.com>
2020-06-25 10:05:45 -0300

    Actualización del código

--- JuegoDeCartasGTK/04-JuegoDeCartasGTK/Principal.cs
+++ JuegoDeCartasGTK/04-JuegoDeCartasGTK/Principal.cs
@@ -19,19 +19,24 @@ namespace JuegoDeCartasGTK
             DeleteEvent += delegate {
                 Application.Quit();
             };
+            
             ModifyBg(StateType.Normal, new Gdk.Color(40, 40, 40));
             Fixed espacio = new Fixed();
             Button btn_01 = new Button("Probando");
             espacio.Put(btn_01, 10, 20);
 
-            btn_01.ModifyFg(StateType.Normal, new Gdk.Color(255, 0, 0));
+            btn_01.ModifyBg(StateType.Normal, new Gdk.Color(255, 0, 0));
 
             Gdk.Pixbuf oro_1;
             try
             {
-                oro_1 = new Gdk.Pixbuf("1_oro.png");
+                oro_1 = new Gdk.Pixbuf("imagen/1_oro.png",85,130);
                 Image imagen = new Image(oro_1);
-                espacio.Put(imagen,50,50);
+
+                espacio.Put(imagen, 50, 50);
+
+                //imagen.HeightRequest = 130;
+
             }
             catch (Exception ex)
             {
