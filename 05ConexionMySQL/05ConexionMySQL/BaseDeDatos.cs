5ca9d95015ef593855332dcb5918fd401654b70a
Luis Sebastián de los Angeles <luissebastianangeles@gmail.com>
2020-07-14 18:49:55 -0300

    Nuevo código: Ejemplo de conexión a mysql

---
+++ 05ConexionMySQL/05ConexionMySQL/BaseDeDatos.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace ConexionMySQL
+{
+    public class BaseDeDatos
+    {
+        public BaseDeDatos()
+        {
+        }
+
+        /***
+         * Crea una nueva conexión a una base de datos     
+         */      
+        public MySqlConnection CrearConexion(string servidor,
+                                             string bdd,
+                                             string usuario,
+                                             string passwd)
+        {
+            MySqlConnection conexion;
+            string datosConexion = "server=" + servidor + ";"+
+                                   "database=" + bdd + ";"+
+                                   "uid=" + usuario + ";"+
+                                   "pwd=" + passwd + ";";
+
+            conexion = new MySqlConnection(datosConexion);
+            return conexion;
+        }
+
+
+
+        public void ProbarConexion(MySqlConnection conexion)
+        {
+            try
+            {
+                conexion.Open();
+                Console.WriteLine("Conexión Exitosa");
+                conexion.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR: " + ex.Message);
+            }
+        }
+
+    }
+}
---
+++ 05ConexionMySQL/05ConexionMySQL/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConexionMySQL
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            BaseDeDatos bdd = new BaseDeDatos();
+
+
+        }
+    }
+}
