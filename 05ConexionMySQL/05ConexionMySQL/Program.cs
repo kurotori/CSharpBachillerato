8e19dde6bf434781c4c6478e7a36ea568dc62a98
Luis Sebastián de los Angeles <luissebastianangeles@gmail.com>
2020-07-14 22:45:45 -0300

    Actualizacion de código

--- 05ConexionMySQL/05ConexionMySQL/Program.cs
+++ 05ConexionMySQL/05ConexionMySQL/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MySql.Data.MySqlClient;
 
 namespace ConexionMySQL
 {
@@ -7,7 +8,9 @@ namespace ConexionMySQL
         public static void Main(string[] args)
         {
             BaseDeDatos bdd = new BaseDeDatos();
-
+            MySqlConnection conexion = bdd.CrearConexion("192.168.4.200","mysql","alumno","");
+            bdd.ProbarConexion(conexion);
+            Console.ReadKey();
 
         }
     }
